93916eb34e96a7f893ac6c1994dbbe362abb676d
zainabayman11 <zainabayman69@gmail.com>
2025-03-16 02:25:15 +0200

    Paginated Queries

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,52 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider , useQuery} from '@tanstack/react-query'
 import Todo from './ToDo/Todo'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // for refetching data when the window is focused , Contraception on the server
-      refetchOnWindowFocus: false,
-    },
-  },
-})
+
 
 function App() {
 
+  // Paginated Queries - dummy data
+
+  const limit = 5
+  const [page, setPage] = useState(0)
+  const Products = async (page) => {
+    const res = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${page * limit}`)
+    const products = await res.json()
+    return products
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['products', page],
+    queryFn: () => Products(page),
+  })
+
+  console.log(data)
+
+ if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      <Todo /> 
-      </QueryClientProvider>
+     <button onClick={() => setPage(page - 1)} disabled={page === 0}>Previous</button>
+     <button onClick={() => setPage(page + 1)} disabled={page === Math.ceil(data.total / limit) - 1}>Next</button>
+    <div className="pagination">
+        <span>Current Page: {page + 1}</span>
+       
+        <span>Last Page: {data.total && Math.ceil(data.total / limit)}</span>
+    </div>
+ <div>
+
+      <h2>Products:</h2>
+        {data.products && data.products.map(product => (
+        <div key={product.id}>
+          <h4>{product.title}</h4>
+          <p>{product.description}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  // defaultOptions: {
+  //   queries: {
+  //     // for refetching data when the window is focused , Contraception on the server
+  //     refetchOnWindowFocus: false,
+  //   },
+  // },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
